11b8237b936dce4fa5b73d0221f9c08a56e97ad9
Steven Chau <steven.chau@mac.com>
2012-11-25 14:35:52 -0800

    Spec single filter for surveygampaign

--- spec/surveygizmo/client/survey_campaign_spec.rb
+++ spec/surveygizmo/client/survey_campaign_spec.rb
@@ -13,6 +13,36 @@ describe Surveygizmo::API do
       a_get("/v2/survey/1018301/surveycampaign").
         should have_been_made
     end
+
+    describe "filters" do
+      # Accept Filters
+      context "one filter" do 
+        before do
+         stub_get("/v2/survey/1018301/surveycampaign").
+           with(:query => {
+              "filter[field][0]"=>"status",
+              "filter[operator][0]"=>"=",
+              "filter[value][0]"=>"Active"
+            }).
+           to_return(:body => fixture("survey_campaigns.json"), :headers => {:content_type => "application/json; charset=utf-8"})
+        end
+
+        it "requests the correct resource" do
+          client.survey_campaigns(1018301, :filter => {
+            "field" => "status",
+            "operator" => "=",
+            "value" => "Active"
+          })
+          a_get("/v2/survey/1018301/surveycampaign").
+           with(:query => {
+              "filter[field][0]"=>"status",
+              "filter[operator][0]"=>"=",
+              "filter[value][0]"=>"Active"
+            }).
+            should have_been_made
+        end
+      end
+    end
   end
 
   describe "#survey_campaign" do
